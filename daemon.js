620488fe5018be235208b5a40af1ffcfb53695df
elf Pavlik <perpetual-tripper@wwelves.org>
2014-11-10 04:59:25 +0100

    started abstracting storage

--- daemon.js
+++ daemon.js
@@ -3,6 +3,7 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var UUID = require('uuid');
+var Promise = require('es6-promise').Promise;
 var config = require('./config');
 
 var daemon = express();
@@ -12,43 +13,59 @@ daemon.options('*', cors());
 
 daemon.use(bodyParser.json({ type: 'application/ld+json' }));
 
+var storage = {
+  get: function(uuid){
+    return new Promise(function(resolve, reject){
+      var path = config.profilesDir + '/' + uuid;
+      fs.readFile(path, function(err, data){
+        if(err) reject(err);
+        resolve(data);
+      });
+    });
+  },
+  save: function(uuid, content){
+    return new Promise(function(resolve, reject){
+      var path = config.profilesDir + '/' + uuid;
+      fs.writeFile(path, JSON.stringify(content), function(err, data){
+        if(err) reject(err);
+        resolve(content);
+      });
+    });
+  }
+};
+
 daemon.post('/', function(req, res){
 
   var uuid = UUID.v4();
-  var path = config.profilesDir + '/' + uuid;
-  var uri = 'http://' + config.domain + '/' + uuid;
-  // FIXME handle errors
   var profile = req.body;
-  profile["@id"] = uri;
+  profile["@id"] = 'http://' + config.domain + '/' + uuid;
 
-  fs.writeFile(path, JSON.stringify(profile), function(err, data){
-    if(err){
-      // TODO add error reporting
-      res.send(500);
-    } else {
+  storage.save(uuid, profile)
+    .then(function(data){
       var min = {
-        "@context": profile["@context"],
-        "@id": uri,
-        "@type": profile["@type"]
+        "@context": data["@context"],
+        "@id": data["@id"],
+        "@type": data["@type"]
       };
       res.type('application/ld+json');
       res.send(min);
-    }
-  });
-
+    })
+    .catch(function(err){
+      // TODO add error reporting
+      res.send(500);
+    });
 });
 
 daemon.get('/:uuid', function(req, res){
-  var path = config.profilesDir + '/' + req.params.uuid;
-  fs.readFile(path, function(err, data){
-    if(err){
-      // TODO add error reporting
-      res.send(500);
-    } else {
+  storage.get(req.params.uuid)
+    .then(function(data){
       res.type('application/ld+json');
       res.send(data.toString());
-    }
-  });
+    })
+    .catch(function(err){
+      // TODO add error reporting
+      res.send(500);
+    });
 });
 
 daemon.listen(config.listenOn, function(){
