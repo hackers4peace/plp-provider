d42c2ea797d3093425a323caa399c589edd27bc6
elf Pavlik <perpetual-tripper@wwelves.org>
2014-11-11 23:36:24 +0100

    GET test creates file it expects in data

--- test/get.js
+++ test/get.js
@@ -1,14 +1,22 @@
 var mocha = require('mocha');
 var chai = require('chai');
 var supertest = require('supertest');
+var fs = require('fs');
 
 var config = require('../config');
+
+var fixture = {
+  uuid: '739bd864-d6d3-48a2-af3a-1a81d65d5604',
+  content: {"name":"elf","additionalname":"","description":"LOL","birthDate":"","nationality":"","website":"","image":"","address":[],"memberOf":[],"contactPoint":[],"interest":[],"@type":"Person","@id":"http://localhost:5000/739bd864-d6d3-48a2-af3a-1a81d65d5604"}
+};
+
+fs.writeFileSync(config.profilesDir + '/' + fixture.uuid, fixture.content);
 
 var request = supertest('http://' + config.domain);
 
 describe('GET', function() {
   it('should respond with existing profile', function(done) {
-    var path =  '/739bd864-d6d3-48a2-af3a-1a81d65d5604';
+    var path =  '/' + fixture.uuid;
     request.get(path)
       .expect(200, done);
   });
@@ -16,7 +24,7 @@ describe('GET', function() {
   describe('Content Type', function() {
 
     it('should set JSON-LD', function(done) {
-      var path =  '/739bd864-d6d3-48a2-af3a-1a81d65d5604';
+      var path =  '/' + fixture.uuid;
       request.get(path)
         .expect('Content-Type', /application\/ld\+json/)
         .expect(200, done);
