c5592a529ff18a28242a8a18047549db254aa171
elf Pavlik <perpetual-tripper@wwelves.org>
2014-11-30 11:26:35 +0100

    refactored request handlers

--- daemon.js
+++ daemon.js
@@ -12,6 +12,7 @@ var levelgraph = require('levelgraph');
 
 var Persona = require('./lib/persona');
 var Authorization = require('./lib/authorization');
+var Verification = require('./lib/verification');
 var FileStore = require('./lib/fileStore');
 
 var config = require('./config');
@@ -24,26 +25,14 @@ daemon.options('*', cors());
 daemon.use(bodyParser.json({ type: 'application/json' }));
 daemon.use(bodyParser.json({ type: 'application/ld+json' }));
 
-/**
- * GET requests don't require authentication
- * attribution: http://stackoverflow.com/a/19337607/2968245
- */
-function restricted(fn) {
-  return function(req, res, next) {
-    if (req.method === 'GET' || req.path.match(/\/auth/)) {
-      next();
-    } else {
-      fn(req, res, next);
-    }
-  };
-}
-
 daemon.use(restricted(expressJwt({ secret: config.secrets.jwt, userProperty: 'agent'})));
-
-var authorization = new Authorization(levelgraph('authorizations'));
+daemon.use(addUri());
 
 // TODO refactor storage to dataset.profiles and use LevelGraph
 var storage = new FileStore(config.dataDir);
+
+var authorization = new Authorization(levelgraph('authorizations'));
+var verification = new Verification(storage);
 
 
 daemon.post('/auth/login', function(req, res){
@@ -79,138 +68,134 @@ daemon.post('/auth/logout', function(req, res){
   res.send(200);
 });
 
-
-function authenticated(req) {
-  if(req.agent) {
-    return req.agent.email;
-  } else {
-    return false;
-  }
-}
-
-function authorized(req, auth) {
-  return req.agent.email === auth.object;
-}
-
-
 daemon.get('/:uuid', function(req, res){
-  var uri = 'http://' + config.domain + '/' + req.params.uuid;
-  storage.get(uri)
-    .then(function(data){
-      res.type('application/ld+json');
-      res.send(JSON.stringify(data));
-    })
-    .catch(function(err){
-      // TODO add error reporting
-      console.log(err);
-      var code = 500;
-      // FIXME abstract error for any storage
-      if(err.code === 'ENOENT') code = 404;
-      // TODO implement HTTP 410 if previously deleted #7
-      res.send(code);
-    });
+  storage.get(req.uri)          // -> doc
+  .then(function(doc){
+    res.type('application/ld+json');
+    res.send(JSON.stringify(doc));
+  })
+  .catch(function(err){
+    res.send(statusCode(err), err.message);
+  });
 });
 
+/**
+ * authentication handled by express-jwt
+ */
 daemon.post('/', function(req, res){
-  var profile = req.body;
 
-  if(profile["@id"]) {
-    res.send(409, 'new profile shouldn not have an @id');
-  } else if(!authenticated(req)) {
-    res.send(401);
-  } else {
-    profile["@id"] = 'http://' + config.domain + '/' + UUID.v4();
-    authorization.create(req.agent.email, profile)
-    .then(storage.save)
-    .then(function(profile){
-      var min = {
-        "@context": profile["@context"],
-        "@id": profile["@id"],
-        "@type": profile["@type"]
-      };
-      res.type('application/ld+json');
-      res.send(min);
-    })
-    .catch(function(err){
-      // TODO add error reporting
-      console.log(err);
-      res.send(500);
-    });
-    }
+  verification.new(req)        // -> req
+  .then(generateId)            // -> req
+  .then(authorization.create)  // -> doc
+  .then(storage.save)          // -> doc
+  .then(function(profile){
+    var min = {
+      "@context": profile["@context"],
+      "@id": profile["@id"],
+      "@type": profile["@type"]
+    };
+    res.type('application/ld+json');
+    res.send(min);
+  })
+  .catch(function(err){
+    res.send(statusCode(err), err.message);
+  });
 });
 
+/**
+ * authentication handled by express-jwt
+ */
 daemon.put('/:uuid', function(req, res){
-  var uri = 'http://' + config.domain + '/' + req.params.uuid;
-  var profile = req.body;
-
-  if(profile['@id'] !== uri) {
-    res.send(400, '@id of profile in request does not match requested uri');
-  } else if(!authenticated(req)) {
-    res.send(401);
-  } else {
-    storage.get(profile['@id'])
-    .then(function(old){
-      return new Promise(function(resolve, reject){
-        authorization.get(profile['@id'])
-        .then(function(auth) {
-          if(!authorized(req, auth)) reject('authorization failed');
-          resolve(profile);
-        });
-      });
-    })
-    .then(storage.save)
-    .then(function(){
-      res.send(200);
-    })
-    .catch(function(err){
-      console.log(err);
-      var code = 500;
-      // FIXME abstract error for any storage
-      if(err.code === 'ENOENT') code = 404;
-      if(err === 'authorization failed') code = 403;
-      if(code === 500) {
-        // TODO add error reporting
-      }
-      res.send(code);
-    });
-  }
+  authorize(req)               // -> req
+  .then(verification.target)   // -> doc
+  .then(storage.save)          // -> doc
+  .then(function(){
+    res.send(200, 'updated!');
+  })
+  .catch(function(err){
+    res.send(statusCode(err), err.message);
+  });
 });
 
-daemon.delete('/:uuid', function(req, res){
 
-  var uri = 'http://' + config.domain + '/' + req.params.uuid;
-  if(!authenticated(req)) {
-    res.send(401);
-  } else {
-    storage.get(uri)
-    .then(function(profile){
-      return new Promise(function(resolve, reject){
-        authorization.get(profile['@id'])
-        .then(function(auth) {
-          if(!authorized(req, auth)) reject('authorization failed');
-          resolve(profile['@id']);
-        });
-      });
-    })
-    .then(storage.delete)
-    .then(authorization.delete)
-    .then(function(){
-      res.send(204);
-    })
-    .catch(function(err){
-      console.log(err);
-      var code = 500;
-      // FIXME abstract error for any storage
-      if(err.code === 'ENOENT') code = 404;
-      if(err === 'authorization failed') code = 403;
-      if(code === 500) {
-        // TODO add error reporting
-      }
-      res.send(code);
-    });
-  }
+/**
+ * authentication handled by express-jwt
+ */
+daemon.delete('/:uuid', function(req, res){
+  authorize(req)               // -> req
+  .then(verification.exists)   // -> doc
+  .then(storage.delete)        // -> doc
+  .then(authorization.delete)  // -> null
+  .then(function(){
+    res.send(204, 'deleted!');
+  })
+  .catch(function(err){
+    res.send(statusCode(err), err.message);
+  });
 });
 
 daemon.listen(config.listenOn, function(){
   console.log('listening on: ', config.listenOn);
 });
+
+
+/**
+ * GET requests don't require authentication
+ * attribution: http://stackoverflow.com/a/19337607/2968245
+ */
+function restricted(fn) {
+  return function(req, res, next) {
+    if (req.method === 'GET' || req.path.match(/\/auth/)) {
+      next();
+    } else {
+      fn(req, res, next);
+    }
+  };
+}
+
+/**
+ * TODO get domain from req?
+ */
+function addUri() {
+  return function(req, res, next) {
+    if(req.method !== 'POST') {
+      req.uri = 'http://' + config.domain + req.path;
+    }
+    next();
+  };
+}
+
+function generateId(req) {
+  return new Promise(function(resolve, reject){
+    req.body['@id'] = 'http://' + config.domain + '/' + UUID.v4();
+    resolve(req);
+  });
+}
+
+function authorize(req) {
+  return new Promise(function(resolve, reject){
+    authorization.get(req.uri)
+    .then(function(auth) {
+      if(!auth || req.agent.email !== auth.object) reject('authorization failed');
+      resolve(req);
+    })
+    .catch(reject);
+  });
+}
+
+function statusCode(error) {
+  // debug
+  console.log('statusCode', error);
+
+  var code = 500;
+  if(error === 'authorization failed') code = 403;
+  // FIXME abstract error for any storage
+  if(error.code === 'ENOENT') code = 404;
+  if(error.message === '@id of profile in request does not match requested uri') code = 400;
+  if(error.message === 'new profile shouldn not have an @id') code = 409;
+  if(code === 500) {
+    // TODO add error reporting
+  }
+  return code;
+}
+
